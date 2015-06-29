979db91e88ca16da3e772e239ad2cd4f880ccf5c
MrHuanghanzhang <690612676@qq.com>
2015-06-29 17:18:03 +0800

    first commit

---
+++ HHZ_SwitchViewForiPad/HHZSwitchView.h
@@ -0,0 +1,13 @@
+//
+//  HHZSwitchView.h
+//  HHZ_SwitchViewForiPad
+//
+//  Created by 黄含章 on 15/6/29.
+//  Copyright (c) 2015年 HHZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHZSwitchView : UIView
+
+@end
---
+++ HHZ_SwitchViewForiPad/HHZSwitchView.m
@@ -0,0 +1,21 @@
+//
+//  HHZSwitchView.m
+//  HHZ_SwitchViewForiPad
+//
+//  Created by 黄含章 on 15/6/29.
+//  Copyright (c) 2015年 HHZ. All rights reserved.
+//
+
+#import "HHZSwitchView.h"
+
+@implementation HHZSwitchView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
